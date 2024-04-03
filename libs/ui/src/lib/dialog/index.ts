06351ef6a733e05f774756db3985d3bfc3bde0e7
penrodlol <christian@penrod.contact>
2024-04-02 20:12:28 -0400

    ✨ Dialog

---
+++ libs/ui/src/lib/dialog/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+export { default as Body } from './components/body.astro';
+export { default as Description } from './components/description.astro';
+export { default as Footer } from './components/footer.astro';
+export { default as Header } from './components/header.astro';
+export { default as Root } from './components/root.astro';
+export { default as Title } from './components/title.astro';
